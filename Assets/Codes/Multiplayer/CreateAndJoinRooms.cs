9cee91d18d030118a1dd1004f4958731bf2f50a2
Yunus Emre <yunustc5461@gmail.com>
2024-12-22 16:51:53 +0300

    multi??

--- Assets/Codes/Multiplayer/CreateAndJoinRooms.cs
+++ Assets/Codes/Multiplayer/CreateAndJoinRooms.cs
@@ -7,23 +7,23 @@ using TMPro;
 
 public class CreateAndJoinRooms : MonoBehaviourPunCallbacks
 {
-   public TMP_InputField createInput;
-   public TMP_InputField joinInput;
+     public TMP_InputField createInput;
+     public TMP_InputField joinInput;
 
 
-   public void CreateRoom() 
-   {
-        PhotonNetwork.CreateRoom(createInput.text);
-   }
+     public void CreateRoom()
+     {
+          PhotonNetwork.CreateRoom(createInput.text);
+     }
 
-   public void JoinRoom() 
-   {
-        PhotonNetwork.JoinRoom(joinInput.text);
-   }
+     public void JoinRoom()
+     {
+          PhotonNetwork.JoinRoom(joinInput.text);
+     }
 
-   public override void OnJoinedRoom() 
-   {
-        PhotonNetwork.LoadLevel("control");
-   }
+     public override void OnJoinedRoom()
+     {
+          PhotonNetwork.LoadLevel("ControlRoom");
+     }
 
 }
